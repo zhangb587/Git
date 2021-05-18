4241c9436926403ba55d117b06e2f976571c69ae
zhaoss <2974609455@qq.com>
2021-05-18 19:51:59 +0800

    创建了Thrad类

---
+++ TestMoudle01/src/com/kaikeba/ThreadDemo.java
@@ -0,0 +1,12 @@
+package com.kaikeba;
+
+public class ThreadDemo {
+    /**
+     * @author xiaozhang;
+     * @create 2021/5/18 19:50;
+     */
+    public static void main(String[] args) {
+        int age = 0;
+        System.out.println(age);
+    }
+}
