ec5ac75ebb7d3249646e121a056b67ab17451184
zhaoss <2974609455@qq.com>
2021-05-18 19:08:00 +0800

    添加了属性……

--- TestMoudle01/src/com/kaikeba/Student.java
+++ TestMoudle01/src/com/kaikeba/Student.java
@@ -1,8 +1,11 @@
 package com.kaikeba;
 
 public class Student {
-/**
- *@author xiaozhang;
- *@create 2021/5/18 18:44;
- */
+    /**
+     * @author xiaozhang;
+     * @create 2021/5/18 18:44;
+     */
+   private String name;
+
+
 }
